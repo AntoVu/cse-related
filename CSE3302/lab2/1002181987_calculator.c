954b69d50922699c205276679662448b4e154dad
AntoVu <atv1987@mavs.uta.edu>
2026-03-08 20:27:11 +0000

    c done

--- CSE3302/lab2/1002181987_calculator.c
+++ CSE3302/lab2/1002181987_calculator.c
@@ -1,65 +1,88 @@
 #include <stdio.h>
 
+// Adds two floats together
 float add(float x, float y) {
     return (x + y);
 }
 
+// Subtracts a float from the other
 float subtract(float x, float y) {
     return (x - y);
 }
 
+// Multiplies two floats together
 float multiply(float x, float y) {
     return (x * y);
 }
 
+// Divides a float from the other
 float divide(float x, float y) {
     return (x / y);
 }
 
-
-// TS DONT WORK YET
 int main() {
-    int done = 0;
+    // Variables to check if the entered values are valid
+    int validInput1 = 0;
+    int validInput2 = 0;
+    int validOperation = 0;
+
+    // Input and result variables
     float num1 = 0;
     float num2 = 0;
     char operation;
     float result = 0;
-    while (!done) {
+
+    // Validate first input
+    while (!validInput1) {
         printf("Enter the first value you would like to perform an operation on: ");
         while (scanf("%f", &num1) != 1) {
             printf("Invalid input for first number, please try again: ");
-            getchar();
+            // getchar used to get rid of newline characters, used multiple times later on
+            while (getchar() != '\n');
         };
+        while (getchar() != '\n');
+        validInput1 = 1;
+    }
+
+    // Validate second input
+    while (!validInput2) {
         printf("Enter the second value you would like to perform an operation on: ");
         while (scanf("%f", &num2) != 1) {
             printf("Invalid input for second number, please try again: ");
-            getchar();
+            while (getchar() != '\n');
         };
-        getchar();
+        while (getchar() != '\n');
+        validInput2 = 1;
+    }
+
+    // Validate operation input using switch statement to choose which operation
+    while (!validOperation) {
         printf("Enter the operation you would like to perform (i.e. +, -, *, or /): ");
         scanf("%c", &operation);
         switch(operation) {
             case '+':
                 result = add(num1, num2);
-                done = 1;
+                validOperation = 1;
                 break;
             case '-':
                 result = subtract(num1, num2);
-                done = 1;
+                validOperation = 1;
                 break;
             case '*':
                 result = multiply(num1, num2);
-                done = 1;
+                validOperation = 1;
                 break;
             case '/':
                 if (num2 == 0) {
                     printf("Cannot divide by 0, please try again.\n");
+                    while (getchar() != '\n');
                     break;
                 }
                 result = divide(num1, num2);
-                done = 1;
+                validOperation = 1;
                 break;
             default:
+                while (getchar() != '\n');
                 printf("Invalid operation, please choose again.\n");
         }
     }
